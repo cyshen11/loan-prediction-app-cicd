6d8b21421929859595e557471f35591fb4bd4a9c
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-12 19:49:44 +0800

    created app

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ def main():
     st.title("Loan Prediction App")
 
     st.sidebar.selectbox("Select a model", ["Logistic Regression"])
+
+    st.text_input("Enter the loan amount")
+    st.text_input("Enter the annual income")
+    st.selectbox("Previous loan defaults", ["Yes", "No"])
     # Load the model
     
-
 if __name__ == "__main__":
     main()
