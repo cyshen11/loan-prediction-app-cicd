08030105c85388fa0bd986743ffd84c1abdc119c
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-12 19:38:39 +0800

    init

--- components/logistic_regression.py
+++ components/logistic_regression.py
@@ -1,3 +1,9 @@
 import pickle
-from sklearn.linear_model import LogisticRegression
+from joblib import load
 
+class LogisticRegressionModel:
+    def __init__(self):
+        self.model = load('../models/lr_model.joblib')
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
