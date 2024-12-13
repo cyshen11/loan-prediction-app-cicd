0cd3164fb90dde2cedd9a8b302a9f56de589ac7b
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-13 22:08:53 +0800

    updated to test prepare data

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -15,3 +15,10 @@ def test_valid_model_name():
 def test_invalid_model_name():
     with pytest.raises(ValueError) as exc_info:
         Predictor("Invalid Model Name")
+
+def test_prepare_data():
+    predictor = Predictor("Logistic Regression")
+    data = predictor.prepare_data(10000, 0.05, 100000, "Yes")
+    assert data.shape == (1, 5)
+    assert data.columns.tolist() == ['loan_percent_income', 'loan_int_rate', 'previous_loan_defaults_on_file_Yes', 'previous_loan_defaults_on_file_No', 'person_income']
+    assert data.values.tolist() == [[0.1, 0.05, True, False, 100000]]
