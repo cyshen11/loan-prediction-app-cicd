4ef981e1106304da353ba75142841ee5ef42d86a
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-13 22:06:38 +0800

    updated to test invalid model name

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -2,7 +2,7 @@
 
 import sys
 import os
-
+import pytest
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from components.predictor import Predictor
@@ -11,3 +11,7 @@ from components.logistic_regression import LogisticRegressionModel
 def test_valid_model_name():
     predictor = Predictor("Logistic Regression")
     assert isinstance(predictor.model, LogisticRegressionModel)
+
+def test_invalid_model_name():
+    with pytest.raises(ValueError) as exc_info:
+        Predictor("Invalid Model Name")
